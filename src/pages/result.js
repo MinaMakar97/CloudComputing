ac4c2a640f1d7d413aaeb9c5695c022b65247f22
Mina Makar <minamakar7@gmail.com>
2022-09-29 09:17:01 +0200

    fix result problem page

--- src/pages/result.js
+++ src/pages/result.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Header from "../component/header";
-import colosseo from "../image/Colosseum_1.jpg";
 import Points from "../component/points";
 
 import "./result.css";
@@ -13,19 +12,19 @@ class Result extends React.Component {
 					<Header></Header>
 				</div>
 				<div className="row inferiore">
-					<div className="row description elem">
-						<p className="titles">
-							<strong>{this.props.contentPage["Item"]["monument_name"]}</strong>
-						</p>
+					<div className="col-12 titles order-1 order-sm-1">
+						<strong>{this.props.contentPage["Item"]["monument_name"].replaceAll("_", " ")}</strong>
+					</div>
+					<div className="col-12 description order-3 order-sm-2">
 						<p className="content_description">{this.props.contentPage["Item"]["attributes"]["description"]}</p>
 					</div>
-					<div className="row content_element">
-						<div className="col-7 elem info">
+					<div className="col-12 col-sm-6 content_element order-4 order-sm-3">
+						<div className="elem info">
 							<Points content={this.props.contentPage}></Points>
 						</div>
-						<div className="col-4 result">
-							<img className="img-result" src={colosseo} alt="description" />
-						</div>
+					</div>
+					<div className="col-12 col-sm-6 result order-2 order-sm-4">
+						<img className="img-result" src={this.props.contentPage["Item"]["attributes"]["url"]} alt="description" />
 					</div>
 				</div>
 			</div>
